3be8423101db82f9e780236ebb230cf988bb7b06
Raihan <r.rafi.aiub@gmail.com>
2024-01-16 16:15:51 +0600

    init 1

--- app/src/main/java/com/example/dailyVideoPlayer/MainActivity.kt
+++ app/src/main/java/com/example/dailyVideoPlayer/MainActivity.kt
@@ -11,37 +11,15 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.dailyVideoPlayer.databinding.ActivityMainBinding
 import com.example.dailyVideoPlayer.ui.theme.DailyVideoPlayerTheme
 
 class MainActivity : ComponentActivity() {
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            DailyVideoPlayerTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
-    }
-}
+        binding = ActivityMainBinding.inflate(layoutInflater)
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    DailyVideoPlayerTheme {
-        Greeting("Android")
+        setContentView(binding.root)
     }
 }
